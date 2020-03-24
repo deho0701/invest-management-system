b39bba9f77971da2721175e8c0d2389888b72738
82109 <82109@DESKTOP-6E79B0U>
2020-03-24 16:07:46 +0900

    수정

---
+++ src/Invest_Management_system.java
@@ -0,0 +1,14 @@
+public class Invest_Management_system {
+
+	public static void main(String[] args) {
+		System.out.println("Invest Distribution system");
+		System.out.println("Select one");
+		System.out.println("1.Add Investment field");
+		System.out.println("2.Delete Investment field");
+		System.out.println("3.Revise Investment field");
+		System.out.println("4.View Investment field");
+		System.out.println("5.Compare with other Investment field");
+
+	}
+
+}
